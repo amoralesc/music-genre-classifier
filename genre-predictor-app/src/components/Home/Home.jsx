db254bc4421575228d55587f3519e13328c822ec
Alejandro Morales <a.moralesc@javeriana.edu.co>
2022-11-09 22:15:35 -0500

    Add new components to Home

--- genre-predictor-app/src/components/Home/Home.jsx
+++ genre-predictor-app/src/components/Home/Home.jsx
@@ -1,9 +1,27 @@
-import Dropzone from "../Dropzone/Dropzone";
+import { useState } from "react";
+
 import Button from "../Button/Button";
+import ToggleButton from "../ToggleButton/ToggleButton";
+import ToggleButtonGroup from "../ToggleButtonGroup/ToggleButtonGroup";
+import Dropzone from "../Dropzone/Dropzone";
+import UploadedFiles from "../UploadedFiles/UploadedFiles";
 
 const Home = () => {
+  const [models, setModels] = useState([]);
+  const [file, setFile] = useState(null);
+
   const handleDrop = (acceptedFiles) => {
-    console.log(acceptedFiles);
+    if (acceptedFiles.length > 0) {
+      setFile(acceptedFiles[0]);
+    }
+  };
+
+  const handleRemoveFile = () => {
+    setFile(null);
+  };
+
+  const handleToggle = (active) => {
+    setModels(active);
   };
 
   return (
@@ -11,9 +29,26 @@ const Home = () => {
       <h1>Music Genre Predictor</h1>
       <p>Go ahead and upload a song to predict its genre!</p>
       <Dropzone onDrop={handleDrop} />
-      <Button variant="primary" onClick={() => console.log("hello world!")}>
-        Hello world!
-      </Button>
+
+      {file && (
+        <>
+          <p>
+            <strong>Uploaded Files</strong>
+          </p>
+          <UploadedFiles files={[file]} removeFile={handleRemoveFile} />
+
+          <p>Now select the models to predict with:</p>
+          <ToggleButtonGroup value={models} onChange={handleToggle} required>
+            <ToggleButton value="mlp">MLP Neural Network</ToggleButton>
+            <ToggleButton value="knn">K-Nearest Neighbors</ToggleButton>
+            <ToggleButton value="svm">Support Vector Machine</ToggleButton>
+          </ToggleButtonGroup>
+
+          <Button variant="primary" onClick={() => console.log("hello world!")}>
+            Upload
+          </Button>
+        </>
+      )}
     </div>
   );
 };

04228bb08081873943eb0e6563bacd3e10c7ccf9
Alejandro Morales <a.moralesc@javeriana.edu.co>
2022-11-23 22:25:09 -0500

    Use alert component in results

--- genre-classifier-app/src/components/Result/Result.jsx
+++ genre-classifier-app/src/components/Result/Result.jsx
@@ -1,29 +1,111 @@
 import { useState, useEffect } from "react";
 
+import "./Result.css";
+
 import firebaseService from "../../services/firebase/firebase";
+import modelService from "../../services/api/model";
 
 import AudioFile from "../AudioFile/AudioFile";
+import Alert from "../Alert/Alert";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 const Result = ({ result }) => {
+  const { id, fileName, storagePath } = result;
+
   const [file, setFile] = useState(null);
+  const [status, setStatus] = useState(result.status);
+  const [prediction, setPrediction] = useState(result.prediction);
 
   useEffect(() => {
     const fetchFile = async () => {
       if (result) {
-        const blob = await firebaseService.downloadFile(result.storagePath);
-        const file = new File([blob], result.fileName, { type: blob.type });
+        const blob = await firebaseService.downloadFile(storagePath);
+        const file = new File([blob], fileName, { type: blob.type });
         setFile(file);
       }
     };
     fetchFile();
-  }, [result, setFile]);
+  }, [fileName, storagePath]);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const result = await modelService.getResult(id);
+      if (result.status.code === "SUCCESS") {
+        clearInterval(interval);
+
+        setStatus(result.status);
+        setPrediction(result.prediction);
+      } else if (result.status.code === "ERROR") {
+        clearInterval(interval);
+
+        setStatus(result.status);
+        setPrediction(null);
+      }
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [id]);
 
   if (!result) return null;
 
+  const { code, message } = status;
+
+  const statusAlertType = () => {
+    switch (code) {
+      case "SUCCESS":
+        return "success";
+      case "ERROR":
+        return "danger";
+      case "PROCESSING":
+        return "info";
+      default:
+        return "info";
+    }
+  };
+
+  if (code === "PROCESSING" || code === "ERROR") {
+    return (
+      <div className="result">
+        <h2>Genre Classification Result</h2>
+        <Alert type={statusAlertType()} message={message} badge={code} />
+      </div>
+    );
+  }
+
+  const { genre, probability } = prediction;
+
   return (
     <div className="result">
-      <h2>Classification Result</h2>
+      <h2>Genre Classification Result</h2>
+
+      <Alert
+        message={message}
+        type={statusAlertType(status)}
+        badge={code}
+        style={{ marginBottom: "1rem" }}
+      />
+
       {file && <AudioFile file={file} />}
+
+      {prediction && (
+        <div className="prediction">
+          <h3>Prediction</h3>
+
+          <table className="table">
+            <tbody>
+              <tr className="table-row">
+                <td className="table-cell dark-cell">Genre</td>
+                <td className="table-cell">{genre}</td>
+              </tr>
+              <tr>
+                <td className="table-cell dark-cell">Probability</td>
+                <td className="table-cell">{probability}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <ProgressBar progress={probability * 100} size="sm" />
+        </div>
+      )}
     </div>
   );
 };
--- genre-classifier-app/src/services/api/model.js
+++ genre-classifier-app/src/services/api/model.js
@@ -1,8 +1,13 @@
 const dummy = {
+  status: {
+    code: "SUCCESS",
+    message: "The model has classified the audio file.",
+  },
   fileName: "audio.wav",
   fileUrl:
     "https://firebasestorage.googleapis.com/v0/b/music-genre-classifier-c0d60.appspot.com/o/audio%2F1668211450469-blues.00000.wav?alt=media&token=b150cc14-12d6-4738-be64-b1384f8a09bd",
   storagePath: "audio/1668211450469-blues.00000.wav",
+  date: "2021-07-18T12:00:00.000Z",
   prediction: {
     genre: "blues",
     probability: 0.95,
